13b4fa209b6d7caa0debe34dd2334f1d6e6e8d20
Andrey Zharikov <andrey@zharikov.pro>
2023-09-17 21:42:49 +0600

    todos

--- src/index.ts
+++ src/index.ts
@@ -76,7 +76,7 @@ export default class OverlappingHierarchy<Node> {
     traverseFunction: any,
     depth: typeof Infinity | 1 = Infinity
   ): Set<Node> | undefined => {
-    if (set === undefined || depth === 1) return set;
+    if (set === undefined || depth === 1) return set; // TODO: check and fix aliasing bug - can clear children of node
 
     const traversedNodes = Array.from(set).flatMap((node) =>
       Array.from(traverseFunction(node) || [])
